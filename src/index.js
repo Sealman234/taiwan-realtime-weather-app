cc3f8d165245f3376431eaeec6864ef06a05a607
Sealman <castle2668@gmail.com>
2021-01-07 18:26:37 +0800

    add PWA

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WeatherApp from './WeatherApp';
-
 import './styles.css';
+import WeatherApp from './WeatherApp';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 function App() {
   return <WeatherApp />;
@@ -10,3 +11,14 @@ function App() {
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+// 將 unregister 改為 register
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
